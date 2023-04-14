7064fea63af0db7d300d68f274020aa9940953a7
markdingli18 <103255544+markdingli18@users.noreply.github.com>
2023-04-14 16:19:43 +0200

    parser implementation 2

--- New folder/parser_.py
+++ New folder/parser_.py
@@ -4,16 +4,12 @@ class Parser:
     def __init__(self, tokens):
         self.tokens = tokens
         self.current_index = 0
-        
-###########################################################################################################################################
 
     def parse(self):
         program = []
         while self.current_index < len(self.tokens):
             program.append(self.parse_statement())
         return program
-    
-###########################################################################################################################################
 
     def parse_statement(self):
         if self.match("IDENTIFIER") and self.match("ASSIGNMENT_OPERATOR"):
@@ -23,46 +19,43 @@ class Parser:
             return ("ASSIGNMENT", identifier, value)
         elif self.match("IF"):
             return self.parse_if()
-        else:
-            raise ParserError(f"Invalid statement at token {self.tokens[self.current_index]}")
-        
-###########################################################################################################################################
+
+    def parse_block(self):
+        self.expect("COLON")
+        statements = []
+        while self.current_index < len(self.tokens) and not self.check("ENDIF") and not self.check("ELSE"):
+            statements.append(self.parse_statement())
+        return statements
 
     def parse_if(self):
-        self.expect("IF")
-        condition = self.parse_expression()
-        self.expect("COLON")
-
-        statements = []
-        while not self.match("ELIF") and not self.match("ELSE"):
-            statements.append(self.parse_statement())
-
-        if self.match("ELIF"):
-            else_branch = self.parse_if()
-        elif self.match("ELSE"):
-            self.expect("COLON")
-            else_statements = []
-            while self.current_index < len(self.tokens):
-                else_statements.append(self.parse_statement())
-            else_branch = ("ELSE", else_statements)
-        else:
-            else_branch = None
-
-        return ("IF", condition, statements, else_branch)
+        self.expect("OPEN_PAREN")
+        condition = self.parse_condition()
+        self.expect("CLOSE_PAREN")
+        true_branch = self.parse_block()
+        false_branch = None
+        if self.match("ELSE"):
+            false_branch = self.parse_block()
+        return ("IF", condition, true_branch, false_branch)
     
-###########################################################################################################################################
+    def parse_condition(self):
+        left = self.parse_expression()
+
+        while self.match("EQUALITY_OPERATOR") or self.match("RELATIONAL_OPERATOR") or self.match("LOGICAL_OPERATOR"):
+            op = self.previous().token_type
+            right = self.parse_expression()
+            left = (op, left, right)
+
+        return left
 
     def parse_expression(self):
         left = self.parse_term()
 
-        while self.match("PLUS") or self.match("MINUS") or self.match("MUL") or self.match("DIV") or self.match("MOD"):
+        while self.match("PLUS") or self.match("MINUS") or self.match("MUL") or self.match("DIV") or self.match("MOD") or self.match("EQUALITY_OPERATOR") or self.match("RELATIONAL_OPERATOR") or self.match("LOGICAL_OPERATOR"):
             op = self.previous().token_type
             right = self.parse_term()
             left = (op, left, right)
 
         return left
-    
-###########################################################################################################################################
 
     def parse_term(self):
         left = self.parse_factor()
@@ -73,8 +66,6 @@ class Parser:
             left = (op, left, right)
 
         return left
-    
-###########################################################################################################################################
 
     def parse_factor(self):
         if self.match("IDENTIFIER"):
@@ -87,37 +78,44 @@ class Parser:
             return expr
         else:
             raise ParserError(f"Invalid factor at token {self.tokens[self.current_index]}")
-        
-###########################################################################################################################################
 
     def match(self, token_type):
         if self.current_index < len(self.tokens) and self.tokens[self.current_index].token_type == token_type:
             self.current_index += 1
             return True
         return False
-    
-###########################################################################################################################################
 
     def expect(self, token_type):
         if not self.match(token_type):
             raise ParserError(f"Expected '{token_type}', found '{self.tokens[self.current_index].token_type}'")
-        
-###########################################################################################################################################
+
+    def check(self, token_type):
+        return self.current_index < len(self.tokens) and self.tokens[self.current_index].token_type == token_type
 
     def previous(self):
         return self.tokens[self.current_index - 1]
     
-###########################################################################################################################################
+    def parse_statement(self):
+        if self.match("IDENTIFIER") and self.match("ASSIGNMENT_OPERATOR"):
+            identifier = self.previous().lexeme
+            value = self.parse_expression()
+            self.expect("SEMICOLON")
+            return ("ASSIGNMENT", identifier, value)
+        elif self.match("IF"):
+            return self.parse_if()
+        else:
+            raise ParserError(f"Invalid statement at token {self.tokens[self.current_index]}")
 
 class ParserError(Exception):
     pass
 
-###########################################################################################################################################
-
 # Usage:
 source_code = """
-x = 10;
-y = 5;
+x = 1;
+if (x == 1):
+    x = x + 1;
+else:
+    x = x - 1;
 """
 
 try:
